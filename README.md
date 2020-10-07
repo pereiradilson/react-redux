8494b7c1e118e35003183fef8107cbae43ffdce0
Adilson Pereira <adilsonpereiradesenvolvimento@gmail.com>
2020-10-07 19:54:24 -0300

    Finished project with REDUX

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # Project with React Redux
 
-This project is under development.
+<h3 align="center">
+  A simple project with Redux
+</h3>
+
+## Installation NodeJS
+
+Installing [NodeJS](https://nodejs.org/en/download/).
+
+## Usage
+
+```bash
+# Cloning project
+git clone https://github.com/pereiradilson/react-redux.git
+
+# Install dependencies
+yarn
+```
+
+## Run fake back-end
+
+```bash
+# Executing back-end
+yarn server
+```
+
+## Run front-end
+
+```bash
+# Executing front-end
+yarn start
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import { addProductToCart } from '../store/modules/cart/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
 
 interface CatalogItemProps {
@@ -9,9 +10,13 @@ interface CatalogItemProps {
 
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
+
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id);
+  });
   
   const handleAddProductToCart = useCallback(() => {
-    dispatch(addProductToCart(product));
+    dispatch(addProductToCartRequest(product));
   }, [dispatch, product]);
 
   return (
@@ -25,6 +30,10 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
       >
         Comprar
       </button>
+
+      { hasFailedStockCheck && (
+        <span style={{ color: 'red' }}>Sem estoque!</span>
+      ) }
     </article>
   );
 }
